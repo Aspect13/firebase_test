8764fe753821ca8a7c89dd872c8d4072f7b60152
Aspect13 <ad13box@gmail.com>
2023-09-22 11:50:33 +0400

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+console.warn('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+console.warn('test module loaded', 'version: 1.0')
+console.warn('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+
+module.exports = {
+    nav: {buttons: []},
+    name: "FB test name",
+    tab: "FB test tab",
+}
